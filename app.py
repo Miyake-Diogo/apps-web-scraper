16026615919d9fe5bba5532ac7a5d567d5b4389d
Diogo Miyake <diogo.miyake@xpi.com.br>
2020-12-11 12:43:22 -0300

    Added modifications

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ from sklearn.utils import resample
 from PIL import Image
 
 def main():
-    st.title('XRate_App - Seu app de análise de sentimentos do Google Play e AppStore(future)')
+    st.set_page_config(page_title="XRate_App", page_icon=":rocket:", layout="wide", initial_sidebar_state="expanded")
+
+    st.title(':rocket: XRate_App - Seu app de análise de sentimentos do Google Play e AppStore(future) :rocket:')
     page = st.sidebar.selectbox("Choose a page", ["Home", "Exploration", "Sentiment Analiser"])
     app_reviews_df = pd.read_csv("Data/xp_google_play_reviews.csv")
     app_reviews_df['at'] = pd.to_datetime(app_reviews_df['at'], errors='coerce')
@@ -52,7 +54,7 @@ def main():
         
         image = Image.open('Data/XDATA.jpeg')
         st.image(image, caption='XDATA - XP INC.', use_column_width=True)
-        
+
         st.text("O Xrate App veio para melhorar suas decisões baseadas em reviews da AppStore e do GooglePlay.")
         st.text("Existem três paginas até o Momento: Home, Exploration and Sentiment Analiser")
 
@@ -62,42 +64,63 @@ def main():
 
         st.text("Fique a Vontade para dar seu FeedBack")
 
-
     if page == "Exploration":
         st.header("This is your data explorer.")
         st.set_option('deprecation.showPyplotGlobalUse', False)
-        
+        col1, col2 = st.beta_columns(2)
+        with col1:
+            st.header('Quantidade de sentimentos positivos, negativos e neutros')
+        #sentiment_chart = app_reviews_df.groupby(['sentiment']).sentiment.count().reset_index(drop=True)
+            sentiment_table = app_reviews_df.groupby(['sentiment']).sentiment.count()
+            st.table(sentiment_table)
+        with col2:
+            st.header('Quantidade de sentimentos (positivos, negativos e neutros) ao longo do tempo')
+            line_sentiment = app_reviews_df.groupby(['year_month']).agg({'sentiment': 'count'})#.reset_index(drop=True)
+            st.line_chart(line_sentiment)
+
         st.header('Alguns dos comentários:')
+        filtro_df = st.selectbox('Filtre os dados por Score:', ('Entre 4 e 5', '3', 'Entre 2 e 1', 'todos'))
         coments = app_reviews_df[['score', 'content']]
-        coments = coments.sample(20)
-        st.table(coments)
-        st.header('Quantidade de Notas do APP')
+
+        if (filtro_df == 'Entre 4 e 5'):
+            coments = coments[coments['score']>3]
+            coments = coments.sample(20)
+            st.table(coments)
+        elif (filtro_df == 'Entre 2 e 1'):
+            coments = coments[coments['score']<3]
+            coments = coments.sample(20)
+            st.table(coments)
+        elif (filtro_df == '3'):
+            coments = coments[coments['score']==3]
+            coments = coments.sample(20)
+            st.table(coments)
+        else:
+            coments = coments.sample(20)
+            st.table(coments)
+
+        
+
+        st.header('Quantidade de Notas do APP (de 1 a 5)')
         sentiment_chart = app_reviews_df.groupby(['score']).agg({'score': 'count'}).reset_index(drop=True)
         st.bar_chart(sentiment_chart)
 
-        st.header('Quantidade de sentimentos positivos, negativos e neutros')
-        #sentiment_chart = app_reviews_df.groupby(['sentiment']).sentiment.count().reset_index(drop=True)
-        sentiment_table = app_reviews_df.groupby(['sentiment']).sentiment.count()
-        st.table(sentiment_table)
-        #st.bar_chart(sentiment_chart)
-        st.header('Quantidade de sentimentos longo do tempo')
-        line_sentiment = app_reviews_df.groupby(['year_month']).agg({'sentiment': 'count'})#.reset_index(drop=True)
-        st.line_chart(line_sentiment)
-
-
-        ## Media de notas este mês
-        notas_media_agg = app_reviews_df.groupby(['year_month']).agg({'score': 'median'}).reset_index()
-        #st.table(notas_media_agg)
-        notas_media = notas_media_agg.loc[notas_media_agg['year_month'] == '2020-12']
-
-        st.header('Media das notas este mês')
-        st.dataframe(notas_media)
-        ## Quantidade de notas 5
-        notas_5 = app_reviews_df.query("score==5")["year_month"]
-        fig, ax = plt.subplots()
-        ax.hist(notas_5, bins=20, color='#33FFE9')
-        plt.xticks(rotation='vertical')
-        st.pyplot(fig)
+        col3, col4 = st.beta_columns(2)
+        with col3:
+            ## Media de notas este mês
+            st.header('Media das notas por mês')
+            notas_media_agg = app_reviews_df.groupby(['year_month']).agg({'score': 'mean'}).reset_index()
+            st.table(notas_media_agg)
+            #notas_media = notas_media_agg.loc[notas_media_agg['year_month'] == '2020-12']
+            #st.header('Media das notas por mês')
+            #st.dataframe(notas_media)
+        with col4:
+            ## Quantidade de notas 5
+            st.header('Quantidade de notas 5')
+            notas_5 = app_reviews_df.query("score==5")["year_month"]
+            fig, ax = plt.subplots()
+            ax.hist(notas_5, bins=20, color='#33FFE9')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
 
         ## WordCloud
         def get_word_clouds(df):
@@ -117,6 +140,15 @@ def main():
         plt.margins(x=0,y=0)
         plt.show()
         st.pyplot()
+
+        ## Tabelao 
+        st.header('Tabelão - Filtre conforme necessidade')
+        cols = ["reviewId","content", "score", "thumbsUpCount", "reviewCreatedVersion", "at", "replyContent", "repliedAt"]
+        num_rows = st.sidebar.slider('Selecione a quantidade de linhas para o Tabelão', min_value=1, max_value=50)
+        st_multiselect = st.sidebar.multiselect("Selecione as colunas para o tabelão", app_reviews_df.columns.tolist(), default=cols)
+        tabelao_df = app_reviews_df[st_multiselect].head(num_rows)
+        st.table(tabelao_df)
+
 
     ##  Rodando a bodega toda 
     elif page == "Sentiment Analiser":
